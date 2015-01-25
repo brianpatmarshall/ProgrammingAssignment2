e7cf61bb474fa17af7aa6513cf170997029212a9
U-NAM\bmarshall019 <bmarshall019@BMARSHALL01962.us.nam.ad.pwcinternal.com>
2015-01-25 14:37:20 -0800

    Added comments and fixed function name

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##===========================================================================
+## This file contains two functions: 
+##   1. makeCacheMatrix is a helper function that facilitates caching.
+##   2. cacheSolve takes a list returned by makeCacheMatrix and calculates the
+##      inverse of a matrix on its first invocation.
+##===========================================================================
+##===========================================================================
+## This function is a helper function that facilitates caching of lengthy
+## computations.
+##===========================================================================
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -18,11 +23,11 @@ makeCacheMatrix <- function(x = matrix()) {
              getsolve = getsolve)
 }
 
-
-
-## Write a short comment describing this function
-
-cachesolve <- function(x, ...) {
+##===========================================================================
+## This function caches the inverse of a function the first time it is invoked.
+## Subsequent invocations will returned the cached value.
+##===========================================================================
+cacheSolve <- function(x, ...) {
         m <- x$getsolve()
         if(!is.null(m)) {
                 message("getting cached data")
